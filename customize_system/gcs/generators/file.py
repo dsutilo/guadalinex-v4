4afa05ea4fc01d8a6553b668a2a1528fac9da12d
Gumer Coronel <gumer.coronel@jobandtalent.com>
2006-11-15 20:29:09 +0000

    Control file generation semi-complete

--- customize_system/gcs/generators/file.py
+++ customize_system/gcs/generators/file.py
@@ -29,6 +29,7 @@ class ControlGenerator(FileGenerator):
 
         self.__set_name()
         self.__set_author()
+        self.__set_descriptions()
         self.__set_depends()
 
         self.__write_control_file()
@@ -40,17 +41,41 @@ class ControlGenerator(FileGenerator):
 
 
     def __set_author(self):
-        pass
+        author = self.info['author']
+        newcontent = self.control_content.replace('<MANTAINER>', author)
+        self.control_content = newcontent
 
 
     def __set_depends(self):
-        pass
+        depends = self.__parse_deps('/depends')
+        newcontent = self.control_content.replace('<DEPENDS>', depends)
+        self.control_content = newcontent
 
 
     def __write_control_file(self):
         control_file = open(config['source_path'] + '/debian/control', 'w')
         control_file.write(self.control_content)
         control_file.close()
+
+
+    def __parse_deps(self, file):
+        depends_list = open(config['source_path'] + file).readlines()
+    
+        new_depends = []
+        for depend in depends_list:
+            depend = depend.strip()
+            if not depend or depend.startswith('#'):
+                continue
+            name_and_version = depend.split()
+
+            depend_string = name_and_version[0]
+            if len(name_and_version) == 2:
+                depend_string += " (%s)" %(name_and_version[1])
+
+            new_depends.append(depend_string)
+
+        depends = ', '.join(new_depends)
+        return depends
 
 
 
