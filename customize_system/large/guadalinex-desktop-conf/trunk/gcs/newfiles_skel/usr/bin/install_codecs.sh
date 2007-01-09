5d7d5a4b885172da108a441fc5a78e4524a41c17
Gabriel Rodríguez Alberich <grodriguez@yaco.es>
2007-01-09 14:42:57 +0000

    Bug.

--- customize_system/large/guadalinex-desktop-conf/trunk/gcs/newfiles_skel/usr/bin/install_codecs.sh
+++ customize_system/large/guadalinex-desktop-conf/trunk/gcs/newfiles_skel/usr/bin/install_codecs.sh
@@ -2,7 +2,7 @@
 
 if [ `id -u` -eq 0 ];then
 	dpkg -l w32codecs > /dev/null 2>&1;
-	if [ $? -eq 0 ]; then
+	if [ $? -ne 0 ]; then
 
 		zenity --question --title "Instalación de codecs" --text "Se va a proceder a instalar los codecs privativos.\n\n Es necesario que esté conectado a Internet.\n\n¿Desea continuar?" && rm -rf /tmp/w32codecs*.deb && wget -nc -P /tmp http://seveas.imbrandon.com/pool/edgy-seveas/extras/w32codecs_20060611-0.0_i386.deb 2>&1 | zenity --progress --pulsate --title "Instalación de codecs privativos" --text "Descargando 14 MB...\n\nEsto llevará un rato." --auto-close && dpkg -i /tmp/w32codecs_20060611-0.0_i386.deb && rm /tmp/w32codecs_20060611-0.0_i386.deb
 		dpkg -l w32codecs > /dev/null 2>&1;
