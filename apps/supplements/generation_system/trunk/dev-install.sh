df6bfa1db87f70547b0123941ce642580eba47ed
Gumer Coronel <gumer.coronel@jobandtalent.com>
2007-01-15 11:59:25 +0000

    * Changes for shared gsdutils.

--- apps/supplements/generation_system/trunk/dev-install.sh
+++ apps/supplements/generation_system/trunk/dev-install.sh
@@ -2,8 +2,7 @@
 
 # Install for developers (using links)
 DIR=$(pwd)
-mkdir /usr/share/sdg/
-ln -s $DIR/sdg-skel /usr/share/sdg/
+ln -s $DIR/sdg /usr/share/sdg
 ln -s $DIR/sdg_new  /usr/bin
 ln -s $DIR/sdg_build  /usr/bin
 ln -s $DIR/doc /usr/share/doc/sdg
--- apps/supplements/generation_system/trunk/dev-remove.sh
+++ apps/supplements/generation_system/trunk/dev-remove.sh
@@ -3,5 +3,5 @@
 # Remove for develpers (if use links)
 [ -L  /usr/bin/sdg_new ] && rm /usr/bin/sdg_new
 [ -L  /usr/bin/sdg_build ] && rm /usr/bin/sdg_build
-[ -L  /usr/share/doc/sdg ] && rm /usr/share/doc/sdg/
-rmdir /usr/share/sdg
+[ -L  /usr/share/doc/sdg ] && rm /usr/share/doc/sdg
+rm /usr/share/sdg
