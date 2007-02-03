0c4a0c6bd9becf7f4e27d37d7e53855dd0660b54
Ghe Rivero <ghe@rivero.io>
2007-02-03 11:48:56 +0000

    Starting guadalinex branch

---
+++ apps/noodle/librapi2/branches/0.9.3-3guada1/src/rapi_internal.h
@@ -0,0 +1,16 @@
+/* $Id: rapi_internal.h 267 2002-11-03 21:05:30Z twogood $ */
+#ifndef __rapi_internal_h__
+#define __rapi_internal_h__
+
+#include <synce.h>
+
+#if HAVE_CONFIG_H
+#include "rapi_config.h"
+#else
+#error HAVE_CONFIG_H is not defined
+#endif
+
+#include <synce_log.h>
+
+#endif
+
