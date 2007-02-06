8fab36e2ebd70f1f911ee8783fac76eab932b17a
c4rlos <cparra@emergya.es>
2007-02-06 19:19:44 +0000

    * correct way to check if a package is installed

--- customize_system/large/guadalinex-desktop-conf/trunk/gcs/newfiles_skel/usr/bin/install_codecs.sh
+++ customize_system/large/guadalinex-desktop-conf/trunk/gcs/newfiles_skel/usr/bin/install_codecs.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 if [ `id -u` -eq 0 ];then
-	dpkg -l w32codecs > /dev/null 2>&1;
-	if [ $? -ne 0 ]; then
-
+	if [ -z "$(dpkg -l | grep ^ii | awk '{print $2}' | grep ^w32codecs)" ]; then
 		zenity --question --title "Instalación de codecs" --text "Se va a proceder a instalar los codecs privativos.\n\n Es necesario que esté conectado a Internet.\n\n¿Desea continuar?" && rm -rf /tmp/w32codecs*.deb && wget -nc -P /tmp http://seveas.imbrandon.com/pool/edgy-seveas/extras/w32codecs_20060611-0.0_i386.deb 2>&1 | zenity --progress --pulsate --title "Instalación de codecs privativos" --text "Descargando 14 MB...\n\nEsto llevará un rato." --auto-close && dpkg -i /tmp/w32codecs_20060611-0.0_i386.deb && rm /tmp/w32codecs_20060611-0.0_i386.deb
-		dpkg -l w32codecs > /dev/null 2>&1;
-		if [ $? -ne 0 ]; then
+		if [ -z "$(dpkg -l | grep ^ii | awk '{print $2}' | grep ^w32codecs)" ]; then
 			zenity --info --title "Instalación de codecs" --text "Ha habido un error en la descarga. Inténtelo más tarde."
 			rm -rf /tmp/w32codecs_20060611-0.0_i386.deb
 		else
@@ -20,3 +17,4 @@ if [ `id -u` -eq 0 ];then
 else
 	gksudo $0;
 fi
+
